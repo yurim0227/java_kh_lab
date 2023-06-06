71ea03c4816e545803a3168424939cd21b0099f4
yurim0227 <byurim227@gmail.com>
2023-06-06 19:05:31 +0900

    .

---
+++ Chapter15/src/stream/inputstream/FileInputStreamTest2.java
@@ -0,0 +1,18 @@
+package stream.inputstream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamTest2 {
+	public static void main(String[] args) {
+		try (FileInputStream fis = new FileInputStream("input.txt")) {
+			int i;
+			while ((i = fis.read()) != -1) {
+				System.out.println((char) i);
+			}
+			System.out.println("end");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Chapter15/src/stream/outputstream/FileOutputStreamTest1.java
@@ -0,0 +1,17 @@
+package stream.outputstream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamTest1 {
+	public static void main(String[] args) {
+		try(FileOutputStream fos = new FileOutputStream("output.txt")) {
+			fos.write(65);
+			fos.write(66);
+			fos.write(67);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("출력이 완료되었습니다.");
+	}
+}
